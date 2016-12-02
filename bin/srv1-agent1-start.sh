13273af01dbf9aecf8f2af614d7c17bb239e95db
Danil Pismenny <danil@brandymint.ru>
2016-12-02 21:27:32 +0300

    add teamcity-server.sh

--- bin/srv1-agent1-start.sh
+++ bin/srv1-agent1-start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-docker run -it -e SERVER_URL=teamcity.brandymint.ru \
+docker run -d \
+  -e SERVER_URL=teamcity.brandymint.ru \
   --name srv1-agent1 \
   --restart=always \
   -v /opt/teamcity/conf/srv1-agent1:/data/teamcity_agent/conf \
