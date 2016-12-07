80640cee68fd991002f7ad3d39fc8f69a12500d6
Danil Pismenny <danil@brandymint.ru>
2016-12-07 18:10:01 +0300

    use docker brandymint/teamcity-agent

--- bin/srv1-agent1-start.sh
+++ bin/srv1-agent1-start.sh
@@ -2,9 +2,9 @@
 
 docker run -d \
   -e SERVER_URL=teamcity.brandymint.ru \
-  --name srv1-agent1 \
+  --name srv1-agent1-v2 \
   --restart=always \
   -v /opt/teamcity/conf/srv1-agent1:/data/teamcity_agent/conf \
   -v /var/run/postgresql/:/var/run/postgresql/ \
-  -e AGENT_NAME=srv1-agent1 \
-  dapi/teamcity-agent
+  -e AGENT_NAME=srv1-agent1-v2 \
+  brandymint/teamcity-agent
