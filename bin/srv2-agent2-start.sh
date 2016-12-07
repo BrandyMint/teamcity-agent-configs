f502e2160fd081bdb697f9c2400d6c7e53bdd051
Danil Pismenny <danil@brandymint.ru>
2016-12-07 18:16:19 +0300

    minor fixes in srv2-agent2

--- bin/srv2-agent2-start.sh
+++ bin/srv2-agent2-start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker run -it -e SERVER_URL=teamcity.brandymint.ru \
+docker run -e SERVER_URL=teamcity.brandymint.ru \
   --name srv2-agent2 \
   --restart=always \
   -v /opt/teamcity/conf/srv2-agent2:/data/teamcity_agent/conf \
