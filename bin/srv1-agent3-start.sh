4c32cb480dc6c82e6c9557fc2dd691f694e10458
Danil Pismenny <dapi@users.noreply.github.com>
2019-05-08 17:34:06 +0300

    Bang8 (#2)
    * teamcity bang8
    
    * Используем brenaymint/teamcity-agent для alfagenesis
    
    * updates

--- bin/srv1-agent3-start.sh
+++ bin/srv1-agent3-start.sh
@@ -2,10 +2,10 @@
 
 docker run -d \
   -e SERVER_URL=teamcity.brandymint.ru \
-  --name srv1-agent1 \
+  --name srv1-agent3 \
   --add-host teamcity.brandymint.ru:95.216.2.125 \
   --restart=always \
-  -v /opt/teamcity/conf/srv1-agent1:/data/teamcity_agent/conf \
+  -v /opt/teamcity/conf/srv1-agent3:/data/teamcity_agent/conf \
   -v /var/run/postgresql/:/var/run/postgresql/ \
-  -e AGENT_NAME=srv1-agent1 \
+  -e AGENT_NAME=srv1-agent3 \
   brandymint/teamcity-agent
