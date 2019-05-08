c6a172f05203e0b436aa9e1efdd9ea675afd41d6
dapi <danil@brandymint.ru>
2019-05-08 15:49:55 +0300

    updates

--- bin/srv1-agent2-start.sh
+++ bin/srv1-agent2-start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 docker run -d \
-  -e SERVER_URL=teamcity.bang8.ru \
+  -e SERVER_URL=teamcity.brandymint.ru \
   --name srv1-agent2 \
-  --add-host teamcity.bang8.ru:95.216.1.239 \
+  --add-host teamcity.brandymint.ru:95.216.2.125 \
   --restart=always \
   -v /opt/teamcity/conf/srv1-agent2:/data/teamcity_agent/conf \
   -v /var/run/postgresql/:/var/run/postgresql/ \
